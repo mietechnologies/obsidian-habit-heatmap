7f841d18c0a843ec64646c680e2c15637e75617a
Brett Chapin <bachapin@proton.me>
2026-02-16 20:04:38 -0500

    chore: add eslint and obsidian lint config

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+// eslint.config.mjs
+import tsparser from "@typescript-eslint/parser";
+import { defineConfig } from "eslint/config";
+import obsidianmd from "eslint-plugin-obsidianmd";
+
+export default defineConfig([
+  {
+    ignores: ["eslint.config.js", "esbuild.config.mjs"],
+  },
+  ...obsidianmd.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: { project: "./tsconfig.json" },
+      globals: {
+        console: "readonly",
+        window: "readonly",
+      },
+    },
+
+    // You can add your own configuration to override or add rules
+    rules: {
+      // example: turn off a rule from the recommended set
+      "obsidianmd/sample-names": "off",
+      // example: add a rule not in the recommended set and set its severity
+      "obsidianmd/prefer-file-manager-trash-file": "error",
+    },
+  },
+]);
